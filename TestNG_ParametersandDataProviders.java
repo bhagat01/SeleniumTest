52a70063f100bcc1ddf1190b9757f02d5326cd6c
Bhagat Khatiwoda <37310447+bhagat01@users.noreply.github.com>
2018-04-18 12:16:24 -0400

    Add files via upload

---
+++ TestNG_ParametersandDataProviders.java
@@ -0,0 +1,13 @@
+package practiceExercise;
+
+public class TestNG_ParametersandDataProviders {
+	
+	public static int CharToAsCII(final char character) {
+		return(int) character;
+	}
+	public static char ASCIIToChar(final int ascii) {
+		return(char) ascii;
+	
+	}
+	
+  }
